0529d801204a0d71a66b5583bec90895d19b422f
DirgantaraPurnaAntartika <dirgantarapurnaa@gmail.com>
2023-06-15 17:08:47 +0700

    testegesg

---
+++ src/main/java/test/dirgantara/tesjenkins/TesjenkinsApplication.java
@@ -0,0 +1,13 @@
+package test.dirgantara.tesjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/dirgantara/tesjenkins/TesjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package test.dirgantara.tesjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
